c29fdf8af5841757bb6c22cbae928f61c4eb4922
Atharva Patel <atharva100@yahoo.com>
2023-09-15 16:03:09 +0530

    solved is perfect square with binary search

---
+++ typescript/is_perfect_square/solution.ts
@@ -0,0 +1,28 @@
+function mySqrt(x: number): boolean {
+    let left = 1;
+    let right = Math.floor(x / 2);
+    if (x == 0 || x == 1) {
+        return true;
+    }
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (mid * mid == x) {
+            return true;
+        } else if (mid * mid < x && (mid + 1) * (mid + 1) > x) {
+            return false;
+        } else if (mid * mid < x) {
+            left = mid + 1;
+        } else if (mid * mid > x) {
+            right = mid - 1;
+        }
+    }
+}
+
+function isPerfectSquare(num: number): boolean {
+    return mySqrt(num);
+}
+
+console.log(isPerfectSquare(1));
+console.log(isPerfectSquare(4));
+console.log(isPerfectSquare(80));
+console.log(isPerfectSquare(81));
